e22b584d1372830eb0dde5d4fcc8b86047f9b4ab
Emilio Blacksmith <emilioblacksmithlush@gmail.com>
2023-09-23 21:44:13 -0600

    Initial project commit: click and drag with react + typescript

---
+++ click_n_drag_typescript/src/App.tsx
@@ -0,0 +1,11 @@
+import { Title } from "./components/Title";
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="container-main flex">
+        <Title />
+    </div>
+  )
+}
+export default App
