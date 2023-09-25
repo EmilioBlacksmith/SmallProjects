04c2d5c636eff0d3a3eb6169a47fa1ac83b1b3f3
Emilio Blacksmith <emilioblacksmithlush@gmail.com>
2023-09-25 16:07:18 -0600

    click_n_drag testsing with Jest || Library Project initial commit and styling

---
+++ click_n_drag_typescript/src/__test__/App.spec.tsx
@@ -0,0 +1,22 @@
+// src/__ tests __/App.test.tsx
+
+class Stack {
+  constructor() {
+    this.top = -1;
+    this.items = {};
+  }
+}
+
+describe('My Stack',() => {
+
+  it('is created empty', () => {
+    const stack = new Stack();
+
+    expect(stack.top).toBe(-1);
+    expect(stack.items).toEqual({});
+  });
+
+  it.todo('can push to the top');
+
+});
+
