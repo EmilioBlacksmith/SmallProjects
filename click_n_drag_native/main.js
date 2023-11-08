4a8df4ae769f5c2c33dcba01375dc17941bed646
Emilio Blacksmith <emilioblacksmithlush@gmail.com>
2023-11-08 17:18:06 -0600

    update all projects

--- click_n_drag_native/main.js
+++ click_n_drag_native/main.js
@@ -4,7 +4,7 @@ document.querySelector('#app').innerHTML = `
   <div>
     <h1>Click and Drag, with Native JS 🤠!</h1>
     <div class="card">
-      <div class="draggableCard" id="draggable">Draggable</div>
+      <div class="draggableCard" id="draggable" draggable="true">Draggable</div>
     </div>
   </div>
 `
