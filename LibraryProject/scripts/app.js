6e8b15b80b8c5fc9468d29a8c2d8bf3c5853bd8a
Emilio Blacksmith <emilioblacksmithlush@gmail.com>
2023-11-09 23:17:40 -0600

    feature: update bookshelf with starting book array

--- LibraryProject/scripts/app.js
+++ LibraryProject/scripts/app.js
@@ -16,4 +16,45 @@ function showNewBookForm() {
 newBookButton.addEventListener("click", showNewBookForm);
 cancelButton.addEventListener("click", showNewBookForm);
 
-// 
+// Library Storage
+
+const booksContainer = document.getElementById("bookshelf");
+let bookShelf = [];
+
+function Book(title, author, year, pages, read, id){
+    this.title = title;
+    this.author = author;
+    this.year = year;
+    this.pages = pages;
+    this.read = read;
+    this.id = id;
+}
+
+bookShelf.push(new Book("Atomic Habits", "James Clear", 2018, 320, "unchecked", bookShelf.length));
+bookShelf.push(new Book("Example Book 2", "Example Author 2", 2023, 420, "checked", bookShelf.length));
+
+function updateBookshelf() {
+    
+
+    for(let i = 0; i < bookShelf.length; i++){
+        let newBook = document.createElement('div');
+        newBook.classList.add('BookCard');
+        newBook.innerHTML = 
+        `
+            <div class="BookCard">
+                <div class="bookTitle">` + bookShelf[i].title + `</div>
+                <div class="authorName">By ` + bookShelf[i].author + `</div>
+                <div class="bookYear">Published in ` + bookShelf[i].year + `</div>
+                <div class="bookPages">Pages: ` + bookShelf[i].pages + `</div>
+                <form>
+                    <label for="bookRead">Read? </label>
+                    <input type="checkbox" id="bookRead" `+ bookShelf[i].read +`>
+                </form>
+                <input type="button" id="deleteEntry" value="🗑">
+            </div>
+        `
+        booksContainer.appendChild(newBook);
+    }
+}
+
+updateBookshelf();
