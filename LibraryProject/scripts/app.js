1e1fe1476c8f3a47a00650636f0ac011fc833d07
Emilio Blacksmith <emilioblacksmithlush@gmail.com>
2023-11-09 21:55:03 -0600

    feature: add new book button with form

--- LibraryProject/scripts/app.js
+++ LibraryProject/scripts/app.js
@@ -1,1 +1,17 @@
-const rootBody = document.getElementById('root');
+// Show and Hide New Book Form
+const newBookButton = document.getElementById("newBookButton");
+const newBookForm = document.getElementById("newBookForm");
+
+function showNewBookForm() {
+    if(newBookForm.classList.contains("hidden")){
+        newBookForm.classList.remove("hidden");
+        newBookForm.classList.add("show");
+    }else{
+        newBookForm.classList.add("hidden");
+        newBookForm.classList.remove("show");
+    }
+}
+
+newBookButton.addEventListener("click", showNewBookForm);
+
+// 
