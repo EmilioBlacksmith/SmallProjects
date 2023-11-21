b7b1e566aed33e56ef53b73f5fec95caab93377e
Emilio Blacksmith <emilioblacksmithlush@gmail.com>
2023-11-20 20:09:23 -0600

    feature: add task with title || enter key press adds new task

--- ToDoAppVanillaJS/scripts/index.js
+++ ToDoAppVanillaJS/scripts/index.js
@@ -1,15 +1,23 @@
 const AppContainer = document.getElementById("appContainer");
+const newTaskTitle = document.getElementById("newTaskTitle");
 
 function Task(checked, title) {
   this.checked = checked;
   this.title = title;
 }
 
-function CreateTask(title) {
-  let newTask = new Task("unchecked", title);
+function CreateTask() {
+  let newTask = new Task("unchecked", newTaskTitle.value);
   let newTaskObject = document.createElement("div");
   newTaskObject.classList.add("taskCard");
   newTaskObject.setAttribute("draggable", true);
   newTaskObject.innerText = newTask.title;
   AppContainer.appendChild(newTaskObject);
 }
+
+// if enter press when on top of task title input, then create task
+newTaskTitle.addEventListener("keyup", function (event) {
+  if (event.key === "Enter") {
+    CreateTask();
+  }
+});
