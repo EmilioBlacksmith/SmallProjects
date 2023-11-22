c71cc5b1cf16da2acb670c9fe9e69f3450b3e79a
Emilio Blacksmith <emilioblacksmithlush@gmail.com>
2023-11-21 19:00:13 -0600

    feature: can delete a task entry

--- ToDoAppVanillaJS/scripts/index.js
+++ ToDoAppVanillaJS/scripts/index.js
@@ -1,26 +1,20 @@
 const AppContainer = document.getElementById("appContainer");
 const newTaskTitle = document.getElementById("newTaskTitle");
 
-function Task(checked, title) {
+let TaskList = [];
+
+// Task Object Constructor
+function Task(checked, title, id) {
   this.checked = checked;
   this.title = title;
+  this.id = id;
 }
 
 function CreateTask() {
   if (newTaskTitle.value !== "") {
-    let newTask = new Task("unchecked", newTaskTitle.value);
-    let newTaskObject = document.createElement("div");
-    newTaskObject.classList.add("taskCard");
-    newTaskObject.setAttribute("draggable", true);
-    newTaskObject.innerHTML = `
-      <button class="deleteButton">
-        <i class="fa-solid fa-trash"></i>
-      </button>
-      <p>${newTaskTitle.value}</p>
-      <input type="checkbox" />
-    `;
-    newTaskTitle.value = "";
-    AppContainer.appendChild(newTaskObject);
+    let newTask = new Task("unchecked", newTaskTitle.value, TaskList.length);
+    TaskList.push(newTask);
+    UpdateAllTasks();
   } else {
     window.alert("the task needs a title, bro");
   }
@@ -32,3 +26,45 @@ newTaskTitle.addEventListener("keyup", function (event) {
     CreateTask();
   }
 });
+
+// Updating Tasks so the last list changes accordingly
+
+function UpdateAllTasks() {
+  CleanTaskListContainer();
+
+  TaskList.forEach((task) => {
+    let newTaskObject = document.createElement("div");
+    newTaskObject.classList.add("taskCard");
+    newTaskObject.setAttribute("draggable", true);
+    newTaskObject.innerHTML = `
+      <button class="deleteButton" onclick="DeleteTask(${task.id})">
+        <i class="fa-solid fa-trash"></i>
+      </button>
+      <p>${task.title}</p>
+      <input type="checkbox" />
+    `;
+    newTaskTitle.value = "";
+    AppContainer.appendChild(newTaskObject);
+  });
+}
+
+function CleanTaskListContainer() {
+  const currentTasks = document.querySelectorAll(".taskCard");
+  currentTasks.forEach((el) => {
+    el.remove();
+  });
+}
+
+// Delete an entry based on the ID of it
+
+function DeleteTask(id) {
+  if (TaskList.length <= 1) {
+    TaskList.pop();
+  } else {
+    TaskList.splice(id, 1);
+  }
+  UpdateAllTasks();
+}
+
+// TODO: Change whole parent based on object
+// TODO: Click and Drag Sorting Order
